f9383ce8ac7190923aa32ef1cb490c90761c08de
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-11 21:07:35 +0200

    (add) quit button

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
+from ast import Break
 import imp
+from tracemalloc import stop
 from turtle import position
 from ursina import *
 from ursina import PositionLimiter
@@ -7,6 +9,12 @@ app = Ursina()
 
 bar_1 = Entity(model = 'cube', scale = (3,1,0), texture = '43bba1622c60137.png', position = Vec3(0,2,0))
 bar_1_button = Button(model = 'cube', scale = (0.36,0.12), texture = '43bba1622c60137.png', position = Vec3(0,0.246,0))
+
+def action():
+    
+
+bar_2_quit = Entity(model = 'cube', scale = (3,1,0), texture = 'Zrzut ekranu 2022-06-11 205741.png', position = Vec3(0,0,0), on_click = action)
+bar_2_quit_button = Button(model = 'cube', scale = (0.36,0.12), texture = 'Zrzut ekranu 2022-06-11 205741.png', position = Vec3(0,0,0))
 
 
 
