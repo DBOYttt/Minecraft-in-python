854babdbcde226e91a324a8ae94462db46bfd30f
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-12 11:34:34 +0200

    working quit button

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ app = Ursina()
 
 bar_1_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,0.1,0), text = 'Play')
 
-bar_2_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,-0.1,0), text = 'quit')
+bar_2_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,-0.1,0), text = 'quit', on_click = application.quit)
 
 screen = Entity(model = 'cube', texture = 'minecraftjavaeditiontlo.jpg', scale = (15,8))
 
