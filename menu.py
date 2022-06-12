8492dec3ea6c17d5127b7a8f67f346eca65d1ecb
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-12 11:45:45 +0200

    working play button

--- menu.py
+++ menu.py
@@ -5,7 +5,17 @@ from ursina import PositionLimiter
 
 app = Ursina()
 
-bar_1_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,0.1,0), text = 'Play')
+def game_on():
+    bar_1_button.ignore = True
+    bar_1_button.visible = False
+
+    bar_2_button.ignore = True
+    bar_2_button.visible = False
+
+    screen.ignore = True
+    screen.visible = False
+
+bar_1_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,0.1,0), text = 'Play', on_click = game_on)
 
 bar_2_button = Button(model = 'cube', scale = (0.36,0.12), position = Vec3(0,-0.1,0), text = 'quit', on_click = application.quit)
 
