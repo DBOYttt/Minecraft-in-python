ced22910c22707be15e328952af1ff8927b4288c
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-15 16:10:26 +0200

    added voxel to game.py

--- game.py
+++ game.py
@@ -1,7 +1,20 @@
-from ursina import * 
-from menugame import bar_1_button
+from ursina import *
+from ursina import texture
+from ursina.prefabs.first_person_controller import FirstPersonController 
 
-if on_click() == application.quit():
-    app = Ursina()
 
-    app.run()
+app = Ursina()
+
+class Voxel(Button):
+    def __init__(self, position = (0, 0, 0)):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube', 
+            origin_y = .5,
+            texture = 'gray_cube',
+            color = color.color(0, 0, random.uniform(0.9, 1.0)),
+            highlight_color = color.lime,
+        )
+
+app.run()
